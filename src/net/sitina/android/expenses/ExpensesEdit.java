1eccf5b1b7094560eb0529e40f49155421336225
Jirka Šitina <jirka.sitina@gmail.com>
2010-10-10 21:47:38 +0200

    Making it work just like notepad example (frankly speaking - simple rename yet)

--- src/net/sitina/android/expenses/ExpensesEdit.java
+++ src/net/sitina/android/expenses/ExpensesEdit.java
@@ -1,5 +1,93 @@
 package net.sitina.android.expenses;
 
-public class ExpensesEdit {
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+public class ExpensesEdit extends Activity {
+
+	private EditText mTitleText;
+    private EditText mBodyText;
+    private Long mRowId;
+    private ExpensesDbAdapter mDbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDbHelper = new ExpensesDbAdapter(this);
+        mDbHelper.open();
+        setContentView(R.layout.expenses_edit);
+        
+       
+        mTitleText = (EditText) findViewById(R.id.title);
+        mBodyText = (EditText) findViewById(R.id.body);
+      
+        Button confirmButton = (Button) findViewById(R.id.confirm);
+       
+        mRowId = savedInstanceState != null ? savedInstanceState.getLong(ExpensesDbAdapter.KEY_ROWID) 
+                							: null;
+		if (mRowId == null) {
+			Bundle extras = getIntent().getExtras();            
+			mRowId = extras != null ? extras.getLong(ExpensesDbAdapter.KEY_ROWID) 
+									: null;
+		}
+
+		populateFields();
+		
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+
+        	public void onClick(View view) {
+        	    setResult(RESULT_OK);
+        	    finish();
+        	}
+          
+        });
+    }
+    
+    private void populateFields() {
+        if (mRowId != null) {
+            Cursor note = mDbHelper.fetchNote(mRowId);
+            startManagingCursor(note);
+            mTitleText.setText(note.getString(
+    	            note.getColumnIndexOrThrow(ExpensesDbAdapter.KEY_TITLE)));
+            mBodyText.setText(note.getString(
+                    note.getColumnIndexOrThrow(ExpensesDbAdapter.KEY_BODY)));
+        }
+    }
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(ExpensesDbAdapter.KEY_ROWID, mRowId);
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveState();
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        populateFields();
+    }
+    
+    private void saveState() {
+        String title = mTitleText.getText().toString();
+        String body = mBodyText.getText().toString();
+
+        if (mRowId == null) {
+            long id = mDbHelper.createNote(title, body);
+            if (id > 0) {
+                mRowId = id;
+            }
+        } else {
+            mDbHelper.updateNote(mRowId, title, body);
+        }
+    }
+    
 }
